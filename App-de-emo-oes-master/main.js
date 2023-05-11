2d7f67ef4f942f95f5730d6186c3dc7b72637836
TabMacedo1897 <tabata.macedo1897@gmail.com>
2023-05-11 14:22:56 -0300

    App emoções Ana

---
+++ App-de-emo-oes-master/main.js
@@ -0,0 +1,21 @@
+var prediction1=" ";
+var prediction2=" ";
+
+Webcam.set({
+    widht: 350,
+    height:350,
+    imageFormat: 'png',
+    pngQuality: 90,
+})
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function takeSnapshot(){
+
+}
+
+function check(){
+    
+}
